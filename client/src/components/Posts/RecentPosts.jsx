a70e28e1c2c5a36fcf13ff51fb1c4651a14b65a4
Sivaani <sivaanijanaswamy@gmail.com>
2025-05-21 10:04:29 +0530

    add padding to search bar

--- client/src/components/Posts/RecentPosts.jsx
+++ client/src/components/Posts/RecentPosts.jsx
@@ -7,7 +7,7 @@ const RecentPosts = ({ posts, isPostTab, refreshPosts }) => {
 
   return (
     <div className="max-w-4xl mx-auto">
-      <div className="flex items-center mb-6 space-x-3">
+      <div className="flex items-center  p-4 mb-6 space-x-3">
         <SearchAndFilter
           onFilterChange={handleFilterChange}
           onSearchChange={handleSearchChange}
